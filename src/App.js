a599d0e0c47874aff87453be35dea9a07a965ab2
Doug-Silva <silvatec.br@gmail.com>
2023-08-21 22:48:55 -0300

    Criando a listagem de tarefas

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import { MdDelete } from 'react-icons/md';
 import './App.css';
 
 const App = () => {
@@ -7,6 +8,14 @@ const App = () => {
   // tecla Esc
   const ESCAPE_KEY = 27;
 
+  const initialTodos = [
+    { id: 1, title: 'Estudar React', checked: false },
+    { id: 2, title: 'Estudar Inglês', checked: true },
+    { id: 3, title: 'Tocar guitarra', checked: false },
+    { id: 4, title: 'Aprender Python', checked: false },
+  ];
+
+  const [todos] = useState(initialTodos);
   const [value, setValue] = useState('');
 
   // função para apagar com a tecla Esc
@@ -50,6 +59,16 @@ const App = () => {
           onChange={onChange}
           onKeyDown={onKeyDown}
         />
+        <ul className="todo-list">
+          {todos.map((todo) => (
+            <li key={todo.id.toString()}>
+              <span className="todo">{todo.title}</span>
+              <button className="remove" type="button">
+                <MdDelete size={28} />
+              </button>
+            </li>
+          ))}
+        </ul>
       </section>
     </section>
   );
